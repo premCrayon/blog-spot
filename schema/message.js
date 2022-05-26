58c353eba3e7dfc2d4eef9d5ae401a44a4fc3fa1
premsk2201 <premsk2201@gmail.com>
2022-05-26 21:58:03 +0530

    complete chat read

--- schema/message.js
+++ schema/message.js
@@ -8,25 +8,41 @@ const messageSchema = mongoose.Schema({
   createTime: {
     type: Date,
     default: Date.now,
-    },
-    users: Array,
-    senders: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-     },
-//   user: {
-//     type: mongoose.Schema.ObjectId,
-//     ref: "User",
-//     required: true,
-//   },
+  },
+  imageUrl: {
+    type: String,
+  },
+  users: Array,
+  senders: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  recevier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  is_active: {
+    type: Boolean,
+    required: true,
+  },
+  is_read: {
+    type: Boolean,
+    required: true,
+  },
+
 });
-messageSchema.virtual("User", {
+messageSchema.virtual("user", {
   ref: "User",
   localField: "senders",
   foreignField: "_id",
 });
-
+messageSchema.virtual("Ruser", {
+  ref: "User",
+  localField: "recevier",
+  foreignField: "_id",
+});
 
 
 module.exports = mongoose.model("message", messageSchema);
